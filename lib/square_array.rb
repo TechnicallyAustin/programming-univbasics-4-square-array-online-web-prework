cecfc283ca94d85545aac71e82f518da467309a5
Austin Johnson <technicallyaustin@gmail.com>
2019-11-14 19:43:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 def square_array(new_numbers)
   counter = 0
-  numbers = [1,4,8]
+  numbers = [1,2,3]
   while counter < numbers.length do
-    new_numbers.push(numbers[counter])
+    # new_numbers.push(numbers[counter])
+    #This will add my numbers onto the existing numbers
     counter = counter + 1 
   end
   return new_numbers

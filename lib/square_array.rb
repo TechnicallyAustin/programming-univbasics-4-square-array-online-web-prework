e558065c2cae7c1ffeca5b89e328438b01445494
Austin Johnson <technicallyaustin@gmail.com>
2019-11-14 19:27:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  counter = 0 
+  new_numbers = []
+  while counter < numbers.length do
+    new_numbers = numbers[counter] * numbers[counter]
+    counter += 1 
+  end
 end
+    
+    
